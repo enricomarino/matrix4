781b1c26e1e06ede0670679284a0902a19c5a700
Enrico Marino <enrico.marino@email.com>
2012-09-21 19:46:31 +0200

    added matrix4.diff

--- index.js
+++ index.js
@@ -339,7 +339,7 @@
    * @return {Float32Array} matrix
    * @api public
    */
-  
+
   matrix4.sum = function (self, a, b) {
     self[ 0] = a[ 0] + b[ 0];
     self[ 1] = a[ 1] + b[ 1];
@@ -364,4 +364,38 @@
     return self;
   };
 
+  /**
+   * diff
+   * Get the difference of matrix a and b.
+   * 
+   * @param {Float32Array} self matrix
+   * @param {Float32Array} a matrix
+   * @param {Float32Array} b matrix
+   * @return {Float32Array} matrix
+   * @api public
+   */
+
+  matrix4.diff = function (self, a, b) {
+    self[ 0] = a[ 0] - b[ 0];
+    self[ 1] = a[ 1] - b[ 1];
+    self[ 2] = a[ 2] - b[ 2];
+    self[ 3] = a[ 3] - b[ 3];
+
+    self[ 4] = a[ 4] - b[ 4];
+    self[ 5] = a[ 5] - b[ 5];
+    self[ 6] = a[ 6] - b[ 6];
+    self[ 7] = a[ 7] - b[ 7];
+
+    self[ 8] = a[ 8] - b[ 8];
+    self[ 9] = a[ 9] - b[ 9];
+    self[10] = a[10] - b[10];
+    self[11] = a[11] - b[11];
+
+    self[12] = a[12] - b[12];
+    self[13] = a[13] - b[13];
+    self[14] = a[14] - b[14];
+    self[15] = a[15] - b[15];
+
+    return self;
+  };r
 }(this));
